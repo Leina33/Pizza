315a8fd3134600242bc4065fa20ab13800eeb581
Leina33 <dominickrutto@gmail.com>
2019-10-04 18:29:02 +0300

    this code will create the space in div add

--- Js/scripts.js
+++ Js/scripts.js
@@ -1,3 +1,14 @@
-$(document).ready(function(){
-    (".pizza1")
-});
+function order(size,crust,price,topping){
+    this.size = size;
+    this.crust = crust;
+    this.price = price;
+    this.topping = topping;
+};
+var size = ["small","medium","large"];
+var crust = ["cripsy", "thick","thin"];
+var price = [""];
+var topping = ["spinach", "mushroom","peppono"];
+
+order.prototype.price = function(){
+    //loop
+}
