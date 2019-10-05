e92e5fb926f23ece47943b13c44cd9f7896b96ad
Leina33 <dominickrutto@gmail.com>
2019-10-06 00:09:13 +0300

    added click button to each of div ids

--- Js/scripts.js
+++ Js/scripts.js
@@ -1,14 +1,3 @@
-function order(size,crust,price,topping){
-    this.size = size;
-    this.crust = crust;
-    this.price = price;
-    this.topping = topping;
-};
-var size = ["small","medium","large"];
-var crust = ["cripsy", "thick","thin"];
-var price = [""];
-var topping = ["spinach", "mushroom","peppono"];
-
-order.prototype.price = function(){
-    //loop
-}
+$(document).ready(function(){
+$()
+});
