41e67719a321471adda6f09e5fd2fc9ab26052f7
Leina33 <dominickrutto@gmail.com>
2019-10-06 01:09:40 +0300

    This code will create a constructor with object order in scripts.js

--- Js/scripts.js
+++ Js/scripts.js
@@ -1,3 +1,31 @@
+// let allLetters
+// (funtion ($){
+//     allLetters = (letters) = > {
+//         $(#size).html(allLetters)
+//     }
+// })(jQuery)
+
 $(document).ready(function(){
-$()
+$("button#submit").click(function(event){
+    event.preventDefault();
+    var size = $("#size").val;
+    var crust =$("#crust").val;
+    var toppings =$("toppings").val;
+
+    alert(size + crust + topping);
 });
+});
+ //creating a constructor
+ function order(size,crust,toppings){
+     this.size = size;
+     this.crust = crust;
+     this.toppings = toppings;
+     this.price = 0;
+ }
+
+ var size =["small", "medium", "large"];
+ var crust = ["cripsy","stuffed","Gluten-free", "cheese-filled","deep"];
+ var toppings= ["pepperoni","mushroom","onions","bacon","extra cheese", "Green peppes", "spinach"];
+
+ //prototypes
+ 
